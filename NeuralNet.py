0249c57ef910e796b260476b2edcf9cbb652f23b
selenajoetwilliams <selena_williams@brown.edu>
2023-01-17 15:15:34 -0800

    model finished

--- NeuralNet.py
+++ NeuralNet.py
@@ -223,6 +223,10 @@ loss_fn = nn.CrossEntropyLoss
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 # full implmenentation of optimization algorithm
+
+# DEFINING THE TRAINING & TEST LOOP
+
+# defining the training loop
 def train_loop(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
@@ -240,4 +244,30 @@ def train_loop(dataloader, model, loss_fn, optimizer):
             print(f"loss: {loss:>7f} [{current:>5d}/{size:5d}]")
             # question: what does this chunk of code mean?
 
-# defining the training and test loop            
+# defining the test loop
+def test_loop(dataloader, model, loss_fnn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:8f} \n")   
+
+# initializing the loss function optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-----------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
+
